a704427d0c5e31febf71a707aec87b2d1c660843
sitinurainixmn <sitinurainixmn@gmail.com>
2021-07-28 18:46:52 +0800

    CoreDataClass & CoreDataProperties added.

---
+++ VaxGuideSG/Model/Users+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Users+CoreDataClass.swift
+//  
+//
+//  Created by Owrmac on 28/7/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Users)
+public class Users: NSManagedObject {
+
+}
