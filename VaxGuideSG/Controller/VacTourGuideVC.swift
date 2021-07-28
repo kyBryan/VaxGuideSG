3460f3b66e219213f6f6d30f86cb1aaa7b006ec9
kyBryan <88072370+kyBryan@users.noreply.github.com>
2021-07-28 17:05:52 +0800

    Added VacTourGuide SB and VC

---
+++ VaxGuideSG/Controller/VacTourGuideVC.swift
@@ -0,0 +1,29 @@
+//
+//  VacTourGuideVC.swift
+//  VaxGuideSG
+//
+//  Created by bryan on 28/7/21.
+//
+
+import UIKit
+
+class VacTourGuideVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
