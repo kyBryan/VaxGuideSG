3702f4019761671edaeb435a564db3c59c26bac1
kyBryan <88072370+kyBryan@users.noreply.github.com>
2021-07-29 17:25:42 +0800

    Refactor Appointment Storyboard separately, attached AppointmentVC to it.

---
+++ VaxGuideSG/Controller/AppointmentVC.swift
@@ -0,0 +1,21 @@
+//
+//  AppointmentVC.swift
+//  VaxGuideSG
+//
+//  Created by owrmac on 29/7/21.
+//
+
+import UIKit
+
+class AppointmentVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
